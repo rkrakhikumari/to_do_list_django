3a3521a0bcf79899ed252400d9e85876e5307454
rkrakhikumari <rkrakhikumari012@gmail.com>
2025-03-04 14:45:11 +0530

    first commit

---
+++ todo/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Todo(models.Model):
+    srno = models.AutoField(auto_created=True, primary_key=True)
+    title = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
